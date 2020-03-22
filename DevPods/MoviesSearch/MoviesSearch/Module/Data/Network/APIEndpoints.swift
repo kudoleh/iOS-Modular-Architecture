650ecdc9a6b7b885f8952dd5d884ae0e351296f6
Oleh Kudinov <kudoleh@hotmail.com>
2020-03-22 15:37:49 +0000

    Added Data transfer object to request

--- DevPods/MoviesSearch/MoviesSearch/Module/Data/Network/APIEndpoints.swift
+++ DevPods/MoviesSearch/MoviesSearch/Module/Data/Network/APIEndpoints.swift
@@ -9,20 +9,19 @@ import Foundation
 import Networking
 
 struct APIEndpoints {
-    
-    static func getMovies(query: String, page: Int) -> Endpoint<MoviesPage> {
-        
+
+    static func getMovies(moviesRequestDTO: MoviesRequestDTO) -> Endpoint<MoviesResponseDTO> {
+
         return Endpoint(path: "3/search/movie/",
                         method: .get,
-                        queryParameters: ["query": query,
-                                          "page": "\(page)"])
+                        queryParametersEncodable: moviesRequestDTO)
     }
-    
+
     static func getMoviePoster(path: String, width: Int) -> Endpoint<Data> {
-        
+
         let sizes = [92, 154, 185, 342, 500, 780]
         let closestWidth = sizes.enumerated().min { abs($0.1 - width) < abs($1.1 - width) }?.element ?? sizes.first!
-        
+
         return Endpoint(path: "t/p/w\(closestWidth)\(path)",
                         method: .get,
                         responseDecoder: RawDataResponseDecoder())
--- DevPods/Networking/Networking/Module/Endpoint.swift
+++ DevPods/Networking/Networking/Module/Endpoint.swift
@@ -21,21 +21,23 @@ public enum BodyEncoding {
 }
 
 public class Endpoint<R>: ResponseRequestable {
-    
+
     public typealias Response = R
-    
+
     public var path: String
     public var isFullPath: Bool
     public var method: HTTPMethodType
+    public var queryParametersEncodable: Encodable? = nil
     public var queryParameters: [String: Any]
     public var headerParamaters: [String: String]
     public var bodyParamaters: [String: Any]
     public var bodyEncoding: BodyEncoding
     public var responseDecoder: ResponseDecoder
-    
+
     public init(path: String,
          isFullPath: Bool = false,
          method: HTTPMethodType,
+         queryParametersEncodable: Encodable? = nil,
          queryParameters: [String: Any] = [:],
          headerParamaters: [String: String] = [:],
          bodyParamaters: [String: Any] = [:],
@@ -44,6 +46,7 @@ public class Endpoint<R>: ResponseRequestable {
         self.path = path
         self.isFullPath = isFullPath
         self.method = method
+        self.queryParametersEncodable = queryParametersEncodable
         self.queryParameters = queryParameters
         self.headerParamaters = headerParamaters
         self.bodyParamaters = bodyParamaters
@@ -56,17 +59,18 @@ public protocol Requestable {
     var path: String { get }
     var isFullPath: Bool { get }
     var method: HTTPMethodType { get }
+    var queryParametersEncodable: Encodable? { get }
     var queryParameters: [String: Any] { get }
     var headerParamaters: [String: String] { get }
     var bodyParamaters: [String: Any] { get }
     var bodyEncoding: BodyEncoding { get }
-    
+
     func urlRequest(with networkConfig: NetworkConfigurable) throws -> URLRequest
 }
 
 public protocol ResponseRequestable: Requestable {
     associatedtype Response
-    
+
     var responseDecoder: ResponseDecoder { get }
 }
 
@@ -75,15 +79,16 @@ enum RequestGenerationError: Error {
 }
 
 extension Requestable {
-    
+
     func url(with config: NetworkConfigurable) throws -> URL {
 
         let baseURL = config.baseURL.absoluteString.last != "/" ? config.baseURL.absoluteString + "/" : config.baseURL.absoluteString
         let endpoint = isFullPath ? path : baseURL.appending(path)
-        
+
         guard var urlComponents = URLComponents(string: endpoint) else { throw RequestGenerationError.components }
         var urlQueryItems = [URLQueryItem]()
-        
+
+        let queryParameters = try queryParametersEncodable?.toDictionary() ?? self.queryParameters
         queryParameters.forEach {
             urlQueryItems.append(URLQueryItem(name: $0.key, value: "\($0.value)"))
         }
@@ -94,14 +99,14 @@ extension Requestable {
         guard let url = urlComponents.url else { throw RequestGenerationError.components }
         return url
     }
-    
+
     public func urlRequest(with config: NetworkConfigurable) throws -> URLRequest {
-        
+
         let url = try self.url(with: config)
         var urlRequest = URLRequest(url: url)
         var allHeaders: [String: String] = config.headers
         headerParamaters.forEach { allHeaders.updateValue($1, forKey: $0) }
-        
+
         if !bodyParamaters.isEmpty {
             urlRequest.httpBody = encodeBody(bodyParamaters: bodyParamaters, bodyEncoding: bodyEncoding)
         }
@@ -109,7 +114,7 @@ extension Requestable {
         urlRequest.allHTTPHeaderFields = allHeaders
         return urlRequest
     }
-    
+
     private func encodeBody(bodyParamaters: [String: Any], bodyEncoding: BodyEncoding) -> Data? {
         switch bodyEncoding {
         case .jsonSerializationData:
@@ -120,11 +125,18 @@ extension Requestable {
     }
 }
 
-fileprivate extension Dictionary {
+private extension Dictionary {
     var queryString: String {
-        
         return self.map { "\($0.key)=\($0.value)" }
             .joined(separator: "&")
             .addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) ?? ""
+    }
+}
+
+private extension Encodable {
+    func toDictionary() throws -> [String: Any]? {
+        let data = try JSONEncoder().encode(self)
+        let josnData = try JSONSerialization.jsonObject(with: data)
+        return josnData as? [String : Any]
     }
 }
