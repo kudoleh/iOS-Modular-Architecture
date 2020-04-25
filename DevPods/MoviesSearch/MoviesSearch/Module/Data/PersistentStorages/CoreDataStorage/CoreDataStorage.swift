a8310e62c85c827d417136a25cc0e14e2c46f89d
Oleh Kudinov <kudoleh@hotmail.com>
2020-04-25 15:20:17 +0100

    Refactor core data

--- DevPods/MoviesSearch/MoviesSearch/Module/Data/PersistentStorages/CoreDataStorage/CoreDataStorage.swift
+++ DevPods/MoviesSearch/MoviesSearch/Module/Data/PersistentStorages/CoreDataStorage/CoreDataStorage.swift
@@ -27,24 +27,22 @@ public final class CoreDataStorage {
         let container = NSPersistentContainer(name: "Name", managedObjectModel: mom)
         container.loadPersistentStores { _, error in
             if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                // Log to Crashlytics
+                debugPrint("CoreDataStorage Unresolved error \(error), \(error.userInfo)")
             }
         }
         return container
     }()
 
     // MARK: - Core Data Saving support
-
     public func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
                 try context.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                // Log to Crashlytics
+                debugPrint("CoreDataStorage Unresolved error \(error), \((error as NSError).userInfo)")
             }
         }
     }
