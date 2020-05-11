286074a2518b2ff6998c60d64b2fe807c99a6c37
Oleh Kudinov <kudoleh@hotmail.com>
2020-05-12 00:45:38 +0100

    Refactor NSPredicate

--- DevPods/MoviesSearch/MoviesSearch/Module/Data/PersistentStorages/MoviesResponseStorage/CoreDataMoviesResponseStorage.swift
+++ DevPods/MoviesSearch/MoviesSearch/Module/Data/PersistentStorages/MoviesResponseStorage/CoreDataMoviesResponseStorage.swift
@@ -20,7 +20,9 @@ final class CoreDataMoviesResponseStorage {
 
     private func fetchRequest(for requestDto: MoviesRequestDTO) -> NSFetchRequest<MoviesRequestEntity> {
         let request: NSFetchRequest = MoviesRequestEntity.fetchRequest()
-        request.predicate = NSPredicate(format: "query = %@ AND page = %d", requestDto.query, requestDto.page)
+        request.predicate = NSPredicate(format: "%K = %@ AND %K = %d",
+                                        #keyPath(MoviesRequestEntity.query), requestDto.query,
+                                        #keyPath(MoviesRequestEntity.page), requestDto.page)
         return request
     }
 
@@ -43,7 +45,6 @@ extension CoreDataMoviesResponseStorage: MoviesResponseStorage {
         coreDataStorage.performBackgroundTask { context in
             do {
                 let request = self.fetchRequest(for: requestDto)
-
                 let result = try context.fetch(request).first
 
                 completion(.success(result?.response?.toDTO()))
